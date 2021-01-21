0241f5b6026af6d6f03336f2d85109920861191b
cheayoons <wndkd280@daum.net>
2021-01-21 14:19:08 +0900

    commit_abc_results

--- sample/src/main/java/ru/slybeaver/truecalendar/SplashActivity.java
+++ sample/src/main/java/ru/slybeaver/truecalendar/SplashActivity.java
@@ -2,15 +2,24 @@ package ru.slybeaver.truecalendar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 2000;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
 
-        startActivity(new Intent(getApplicationContext(), login.class));
-        finish();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(SplashActivity.this, login.class);
+                startActivity(homeIntent);
+            }
+        },SPLASH_TIME_OUT);
     }
 }
